0d2d1894666444fb4a8a2256972a450fdf8480ed
Artyom Batura <artyombatura@gmail.com>
2020-10-07 20:25:04 +0300

    Initial

---
+++ RxBSUIRSchedule/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxBSUIRSchedule
+//
+//  Created by Artyom Batura on 10/7/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
